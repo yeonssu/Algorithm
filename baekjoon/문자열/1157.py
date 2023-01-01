0a3a315bf4e2714e2e0fff6f9f076f764e0b2419
yeonssu <dustn7197@naver.com>
2023-01-01 17:51:20 +0900

    second commit

--- baekjoon/문자열/1157.py
+++ baekjoon/문자열/1157.py
@@ -1,3 +1,4 @@
+# sol1)
 normal_word = input()       # mississipi
 upper_word = normal_word.upper() 
 word_lst = list(set(upper_word)) # ['m', 'i', 's', 'p']
@@ -12,3 +13,16 @@ if cnt.count(max(cnt)) >= 2:    # max(cnt) = 4 / cnt.count(max(cnt)) = 2
     print("?")
 else:     
     print(word_lst[cnt.index(max(cnt))])
+
+# sol2)
+normal_word = input()
+upper_word = normal_word.upper() 
+alphabet = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+for i in upper_word:
+    alphabet[ord(i)-65] += 1
+
+if alphabet.count(max(alphabet)) >= 2:
+    print("?")
+else:     
+    print(chr(alphabet.index(max(alphabet))+65))
