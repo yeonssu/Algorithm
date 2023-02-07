0fe14b6bf945e8616c05b7518a8b1b4e99624dc6
yeonssu <dustn7197@naver.com>
2023-02-07 22:45:34 +0900

    2/7

--- baekjoon/문자열/5525.py
+++ baekjoon/문자열/5525.py
@@ -5,13 +5,26 @@ S = input()
 # 50점
 # Pn = "IO"*N + "I"
 #
-# cnt = 0
+# answer = 0
 # for m in range(M):
 #     if S[m] == "I":
 #         if m + (2 * N + 1) < M:
 #             if S[m:m + (2 * N + 1)] == Pn:
-#                 cnt += 1
+#                 answer += 1
 #
-# print(cnt)
+# print(answer)
 
+# 100점
+answer, cnt, i = 0, 0, 0
+while i < M:
+    if S[i:i + 3] == "IOI":
+        i += 2
+        cnt += 1
+        if cnt == N:
+            answer += 1
+            cnt -= 1
+    else:
+        i += 1
+        cnt = 0
 
+print(answer)
