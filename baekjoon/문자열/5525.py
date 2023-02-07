49f30b4d219a68ffc8e4242a77a0bdde733c008f
yeonssu <dustn7197@naver.com>
2023-02-07 22:33:19 +0900

    2/7

---
+++ baekjoon/문자열/5525.py
@@ -0,0 +1,17 @@
+N = int(input())
+M = int(input())
+S = input()
+
+# 50점
+# Pn = "IO"*N + "I"
+#
+# cnt = 0
+# for m in range(M):
+#     if S[m] == "I":
+#         if m + (2 * N + 1) < M:
+#             if S[m:m + (2 * N + 1)] == Pn:
+#                 cnt += 1
+#
+# print(cnt)
+
+
