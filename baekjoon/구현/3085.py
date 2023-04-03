f57a853c392e3139f25954d4f9d68fe16a0ccdfa
yeonssu <dustn7197@naver.com>
2023-04-03 16:56:30 +0900

    4/3

---
+++ baekjoon/구현/3085.py
@@ -0,0 +1,47 @@
+import sys
+
+
+def count_candy(N, board):
+    max_cnt = 0
+    for i in range(N):
+        # 아래랑 비교하기
+        cnt = 1
+        for j in range(1, N):
+            if board[j][i] == board[j - 1][i]:
+                cnt += 1
+            else:
+                cnt = 1
+            max_cnt = max(max_cnt, cnt)
+
+        # 오른쪽이랑 비교하기
+        cnt = 1
+        for j in range(1, N):
+            if board[i][j] == board[i][j - 1]:
+                cnt += 1
+            else:
+                cnt = 1
+            max_cnt = max(max_cnt, cnt)
+
+    return max_cnt
+
+
+input = sys.stdin.readline
+N = int(input().strip())
+board = [list(input().strip()) for n in range(N)]
+answer = 0
+
+for i in range(N):
+    for j in range(N):
+        # 오른쪽이랑 바꾸기
+        if i + 1 < N:
+            board[i][j], board[i + 1][j] = board[i + 1][j], board[i][j]
+            answer = max(count_candy(N, board), answer)
+            board[i][j], board[i + 1][j] = board[i + 1][j], board[i][j]
+
+        # 아래랑 바꾸기
+        if j + 1 < N:
+            board[i][j], board[i][j + 1] = board[i][j + 1], board[i][j]
+            answer = max(count_candy(N, board), answer)
+            board[i][j], board[i][j + 1] = board[i][j + 1], board[i][j]
+
+print(answer)
