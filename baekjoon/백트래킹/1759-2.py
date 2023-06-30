03ba34876a774140fa2708e1a810244400d9bd13
yeonssu <dustn7197@naver.com>
2023-06-30 21:09:31 +0900

    6/30

--- baekjoon/백트래킹/1759-2.py
+++ baekjoon/백트래킹/1759-2.py
@@ -1,6 +1,4 @@
-
 import sys
-from itertools import combinations
 input = sys.stdin.readline
 
 L, C = map(int, input().strip().split())
@@ -13,9 +11,16 @@ L, C = map(int, input().strip().split())
 chars = list(map(str, input().strip().split()))
 chars = sorted(chars)
 
-# def solve(result):
-#     if len(result) == L and :
-#         print(result)
-#         result = ""
-#         exit()
-#
+
+def solve(index, moeum, jaeum, result):
+    if len(result) == L and moeum >= 1 and jaeum >= 2:
+        print(result)
+        return
+    for i in range(index, C):
+        if chars[i] == "a" or chars[i] == "e" or chars[i] == "i" or chars[i] == "o" or chars[i] == "u":
+            solve(i + 1, moeum + 1, jaeum, result + chars[i])
+        else:
+            solve(i + 1, moeum, jaeum + 1, result + chars[i])
+
+
+solve(0, 0, 0, "")
