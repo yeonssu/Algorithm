d55c1b2ace36f13107b094e7cbf27803d7cbcecb
yeonssu <dustn7197@naver.com>
2023-09-20 17:32:04 +0900

    9/20

--- baekjoon/구현/21608.py
+++ baekjoon/구현/21608.py
@@ -14,42 +14,51 @@ if __name__ == "__main__":
         a, b, c, d, e = map(int, input().strip().split())
         favorite[a] = [b, c, d, e]
     for student in favorite.keys():
-        friends = list(favorite.get(student))
+        friends = favorite.get(student)
         possible_seat = []
-        for i in range(N):
-            for j in range(N):
+        for y in range(N):
+            for x in range(N):
                 like = 0
                 blank = 0
-                if classroom[i][j] == 0:
+                if classroom[y][x] == 0:
                     for k in range(4):
-                        nx = j + dx[k]
-                        ny = i + dy[k]
+                        nx = x + dx[k]
+                        ny = y + dy[k]
                         if 0 <= nx < N and 0 <= ny < N:
                             if classroom[ny][nx] in friends:
                                 like += 1
                             if classroom[ny][nx] == 0:
                                 blank += 1
-                    possible_seat.append((like, blank, i, j))
-        possible_seat.sort(key=lambda x:(-x[0], -x[1], x[2], x[3]))
-        like, blank, i, j = possible_seat[0]
-        classroom[i][j] = student
+                    possible_seat.append((like, blank, x, y))
+        possible_seat.sort(key=lambda x: (-x[0], -x[1], x[3], x[2]))
+        like, blank, x, y = possible_seat[0]
+        classroom[y][x] = student
 
-    # 그 학생과 인접한 칸에 앉은 좋아하는 학생의 수를 구해야 한다.
-    # 그 값이 0이면 학생의 만족도는 0, 1이면 1, 2이면 10, 3이면 100, 4이면 1000이다.
+    # 좋아하는 학생의 수 0 : 만족도 0
+    # 좋아하는 학생의 수 1 : 만족도 1
+    # 좋아하는 학생의 수 2 : 만족도 10
+    # 좋아하는 학생의 수 3 : 만족도 100
+    # 좋아하는 학생의 수 4 : 만족도 1000
     result = 0
     for student in range(1, N * N + 1):
         friends = favorite.get(student)
-        for i in range(N):
-            for j in range(N):
-                if classroom[i][j] == student:
-                    answer = 0
+        for y in range(N):
+            for x in range(N):
+                if classroom[y][x] == student:
+                    cnt = 0
                     for k in range(4):
-                        nx, ny = j + dx[k], i + dy[k]
+                        nx = x + dx[k]
+                        ny = y + dy[k]
                         if 0 <= nx < N and 0 <= ny < N:
                             if classroom[ny][nx] in friends:
-                                answer += 1
-                    if answer != 0:
-                        result += 10 ** (answer - 1)
+                                cnt += 1
+                    if cnt == 1:
+                        result += 1
+                    elif cnt == 2:
+                        result += 10
+                    elif cnt == 3:
+                        result += 100
+                    elif cnt == 4:
+                        result += 1000
 
     print(result)
-
